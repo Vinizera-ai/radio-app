4ca024c6544afb4b8d1e4db8e7089f207931ec3b
Vinicius Castro <mvinidc@gmail.com>
2025-07-31 08:33:57 -0300

    Add placeholder screencast integration

---
+++ services/screencast.js
@@ -0,0 +1,6 @@
+export async function startScreencast(streamUrl) {
+  console.log('Screencast requested for', streamUrl);
+  // TODO: integrate react-native-google-cast or other screencast library.
+  // This is a placeholder implementation because the actual
+  // native module cannot be installed in this environment.
+}
